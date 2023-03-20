6a2181b0fb0b59a30f0a6566c77d69d8ee3265c2
Destiny Marshall <34584955+Marshalllife@users.noreply.github.com>
2023-03-20 04:33:20 +0100

    todo init

---
+++ todo/src/Todo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { ACTIONS } from './App'
+
+export default function Todo({ todo, dispatch }) {
+    return (
+        <div>
+            <span style={{ color: todo.complet ? '#000' : '#fff' }}>{todo.name}
+            </span>
+            <button onClick={() => dispatch({ type: ACTIONS.TOGGLE_TODO, payload: { id: todo.id } })}>Toggle</button>
+            <button onClick={() => dispatch({ type: ACTIONS.DELETE_TODO, payload: { id: todo.id } })}>Delete</button>
+        </div>
+    )
+}
